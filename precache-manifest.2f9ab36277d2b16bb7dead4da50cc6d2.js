782d292552d21af8bf58fbc52ef51aa72d6f6eef
tommy8808 <tommyguy8808@gmail.com>
2020-02-15 23:53:28 +0900

    Updates

---
+++ precache-manifest.2f9ab36277d2b16bb7dead4da50cc6d2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "307e1df06f20a44c99dce4e8e5ac1595",
+    "url": "/movie_app/index.html"
+  },
+  {
+    "revision": "07af5a3d86840e7baaf8",
+    "url": "/movie_app/static/css/main.64b2c200.chunk.css"
+  },
+  {
+    "revision": "70e84318d65e22caa855",
+    "url": "/movie_app/static/js/2.1c932c0d.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/movie_app/static/js/2.1c932c0d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "07af5a3d86840e7baaf8",
+    "url": "/movie_app/static/js/main.01c4c589.chunk.js"
+  },
+  {
+    "revision": "ca4b155c3570fabe1dd4",
+    "url": "/movie_app/static/js/runtime-main.98d66c69.js"
+  }
+]);
